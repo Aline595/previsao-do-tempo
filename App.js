f101ded9995b54843f547e1bb95e1ea3f702288c
unknown <aline.as385@gmail.com>
2020-09-11 16:25:38 -0300

    inicial

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput, Button, Keyboard } from 'react-native';
 
 export default function App() {
+
+  const [cidade, setCidade] = useState('');
+  const [previsoes, setPrevisoes] = useState([]);
+  
+  const capturarCidade = (cidade) => {
+    setCidade(cidade);
+  }
+
+  const obtemPrevisoes = () => {
+    setPrevisoes([]);
+    const target = endPoint + cidade + "&appid=" + apiKey;
+    fetch(target)
+    .then((dados) => dados.json())
+    .then((dados) => {
+      setPrevisoes(dados['list'])
+      Keyboard.dismiss()
+    });
+  }
+
+  const endPoint = "https://api.openweathermap.org/data/2.5/forecast?lang=pt_br&units=metric&q=";
+  const apiKey = "e2014975b82e748e2bf1d191ff8f31b4";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entrada}>
+        <TextInput 
+          style={styles.nomeCidade}
+          value={cidade}
+          onChangeText={capturarCidade}
+        />
+        <Button 
+          title="OK"
+          onPress={obtemPrevisoes}
+        />
+      </View>
+      <FlatList 
+        data={previsoes}
+        renderItem={ previsao => (
+          <Text>{JSON.stringify(previsao)}</Text>
+        )}
+      />
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 40,
+    flexDirection: 'column',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    
+  },
+  entrada: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    marginBottom: 8,
+  },
+  nomeCidade:{
+    padding: 12,
+    borderBottomColor: '#BB96F3',
+    borderBottomWidth: 2,
+    textAlign: 'left',
+    flexGrow: 0.9
+
   },
 });
---
+++ components/PrevisaoItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Cartao from './Cartao';
+
+const PrevisaoItem = (props) => {
+  return (
+    <Cartao  >
+
+    </Cartao>
+  )
+}
+
+const estilos = StyleSheet.create({
+  cartao: {
+    marginBottom: 
+  }
+});
+
+export default PrevisaoItem;
